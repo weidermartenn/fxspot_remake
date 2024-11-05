2e055f655e77ac2013a08992e0b427bf61a7f315
weidermartenn <weidermartenn@gmail.com>
2024-11-05 14:38:56 +0300

    layout update

--- src/views/Main/ui/MainPage.vue
+++ src/views/Main/ui/MainPage.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="h-[calc(100vh-75px)]">
-
+    <div class="block h-[calc(100vh-75px)]">
+        <div class="flex flex-col text-center items-center gap-4">
+            <span class="text-3xl p-6 w-[34rem] border-b-2 border-emerald-800">Добро пожаловать на <b class="text-emerald-800">FXSpot</b></span>
+            <p class="w-1/3 mx-auto">Узнайте какая сейчас погода в вашем городе, читайте новости и изучайте актуальный курс валют</p>
+        </div>
     </div>
 </template>
 
